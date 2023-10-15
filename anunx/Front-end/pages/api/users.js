c60ad609e5b2d4ecf4dd7df96d01cb315be19360
Stefan Picciani <stefancorporate@outlook.com>
2023-10-15 15:40:58 +0000

    criptografando senha

--- anunx/Front-end/pages/api/users.js
+++ anunx/Front-end/pages/api/users.js
@@ -1,4 +1,6 @@
 import dbConnect from "@/src/utils/dbConnect";
+import UsersModel from '../../src/models/users';
+import { crypto } from '../../src/utils/password';
 
 const users = async (req, res) => {
     const { method} = req
@@ -8,6 +10,32 @@ const users = async (req, res) => {
             await dbConnect()
             res.status(200).json({ success: true })
             break
+
+        case 'POST': 
+            // PEGAR DADOS QUE VEM NO REQ
+            //conectar ao banco
+            //criptografar a senha
+            //salvar dados
+            // responder sucesso
+            const {
+                name,
+                email,
+                password,
+            } = req.body
+
+            await dbConnect()
+
+            const passwordCrypto = await crypto(password)
+
+            const user = new UsersModel({
+                name,
+                email,
+                password: passwordCrypto,
+            })
+
+           await user.save()
+
+            res.status(201).json({ success: true })
     }
 };
 
