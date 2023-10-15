7ebf379d2257f6e60fba2793403a3afa0c05d5a3
Stefan Picciani <stefancorporate@outlook.com>
2023-10-15 15:06:55 +0000

    adicionando conexão com o banco de dados

--- anunx/Front-end/src/utils/Env.js
+++ anunx/Front-end/src/utils/Env.js
@@ -1,19 +1,19 @@
-import { Environment } from "../enums/Environment";
+// import { Environment } from "../enums/Environment";
 
 
-const dev ={
-    url: {
-        API_URL: "https://00000/api/",
-    },
-};
+// const dev ={
+//     url: {
+//         API_URL: "https://00000/api/",
+//     },
+// };
 
-const prod = {
-    url: {
-        API_URL: "https://anunx.api.devtech.io/api/",
-    },
-};
+// const prod = {
+//     url: {
+//         API_URL: "https://anunx.api.devtech.io/api/",
+//     },
+// };
 
 
-export const config = 
-    process.env.NODE_ENV === Environment.DEVELOPMENT ? dev : prod;
+// export const config = 
+//     process.env.NODE_ENV === Environment.DEVELOPMENT ? dev : prod;
 
